05af00b212c7e2d94c0c564864f195ff50af462a
snooze <89538185+snooze789@users.noreply.github.com>
2022-07-31 23:35:57 -0400

    Add project files.

---
+++ mcdonld simulation/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mcdonld_simulation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            outside_mcd.Visibility = Visibility.Hidden;
+            go_in_mcd.Visibility = Visibility.Hidden;
+            cheeseburger.Visibility = Visibility.Visible;
+            cheeseburg.Visibility = Visibility.Visible;
+            mic_chicken_balls.Visibility = Visibility.Visible;
+            mc_chicken_buttn.Visibility = Visibility.Visible;
+        }
+        private void cheeseclick(object sender, RoutedEventArgs e)
+        {
+            cheesemcd.Visibility = Visibility.Visible; 
+            cheeseburger.Visibility = Visibility.Hidden;
+            cheeseburg.Visibility = Visibility.Hidden;
+
+        }
+        private void mcchicken(object sender, RoutedEventArgs e)
+        {
+            cheesemcd.Visibility = Visibility.Hidden;
+            cheeseburger.Visibility = Visibility.Hidden;
+            cheeseburg.Visibility = Visibility.Hidden;
+            mic_chicken_balls.Visibility = Visibility.Hidden;
+            mc_chicken_buttn.Visibility = Visibility.Hidden;
+            mcchick.Visibility = Visibility.Visible;
+
+        }
+    }
+}
+
